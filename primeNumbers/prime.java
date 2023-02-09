0a8f66c9b9e0cca9875404a1ee483ea4ababf265
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-02-09 23:46:13 +0530

    prime Numbers

---
+++ primeNumbers/prime.java
@@ -0,0 +1,40 @@
+package primeNumbers;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class prime {
+    public static void main (String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the number to check prime or not: ");
+        int num = sc.nextInt();
+
+        primeNum(num);
+        
+    }
+
+    
+    private static void primeNum(int num) {
+
+        int count = 0;
+        
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                count++;
+            }
+        }
+
+        if(count==0){
+            System.out.println("The number is prime");
+        }
+
+        else{
+            System.out.println("The number is not prime");
+        }
+    }
+
+
+    
+}
